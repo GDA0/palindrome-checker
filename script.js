4395b4663a3963ab6f2cea513993625f4473208d
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-16 13:11:38 +0000

    Solve user stories to ensure all tests are passed

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Form validation
-const form = document.querySelector(".needs-validation");
-form.addEventListener(
-	"submit",
-	(event) => {
-		if (!form.checkValidity()) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-
-		form.classList.add("was-validated");
-	},
-	false
-);
-
 // Automatically collapse hamburger after a .nav-link is clicked
 const navbarCollapse = document.querySelector(".navbar-collapse");
 const navLinks = document.querySelectorAll(".nav-link");
@@ -25,3 +10,36 @@ navLinks.forEach((link) => {
 		bsCollapse.hide();
 	});
 });
+
+const formElement = document.querySelector("form");
+
+formElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const userInput = formElement.querySelector("input").value;
+
+	checkPalindrome(userInput);
+});
+
+// Function to check if the user input is a palindrome
+const checkPalindrome = (userInput) => {
+	// Check if the input is empty.
+	if (userInput.trim() === "") {
+		alert("Please input a value");
+		return;
+	}
+
+	// Regex to remove non-alphanumeric characters
+	const regex = /[^\w]|_/g;
+
+	const processedInput = userInput.replace(regex, "").toLowerCase();
+	const reversedInput = processedInput.split("").reverse().join("");
+
+	const resultDisplay = document.querySelector("#result");
+
+	if (processedInput === reversedInput) {
+		resultDisplay.innerHTML = `<strong>${userInput}</strong> is a palindrome.`;
+	} else {
+		resultDisplay.innerHTML = `<strong>${userInput}</strong> is not a palindrome.`;
+	}
+};
